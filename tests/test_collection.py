5d2ef9a5ca682d5942525bb91009a61ef4e926c8
James Walker <james@gothicanatomist.co.uk>
2025-01-16 22:12:19 +0000

    TEST: Collection and Reversible extension

--- tests/test_collection.py
+++ tests/test_collection.py
@@ -7,7 +7,11 @@ from typed_collection import TypedCollection
 
 class IntCollection(TypedCollection[int]):
     """Dummy typed collection with a generic type of Integer"""
+    pass
 
+
+class StrCollection(TypedCollection[str]):
+    """Dummy typed collection with a generic type of String"""
     pass
 
 
@@ -30,9 +34,21 @@ def valid_int_list() -> List[int]:
 
 
 @pytest.fixture
+def valid_int_list_again() -> List[int]:
+    """Returns a list of valid integer values (again)."""
+    return [6, 8, 10, 12, 13]
+
+
+@pytest.fixture
 def invalid_int_list() -> List:
     """Returns a list of valid integer values and an invalid string value."""
     return [1, 2, 3, "This is a string", 5, 26]
+
+
+@pytest.fixture
+def valid_str_list() -> List[str]:
+    """Returns a list of valid string values."""
+    return ["Once", "Upon", "a", "midnight", "dreary"]
 
 
 def test_collection_init(valid_int_list):
@@ -111,3 +127,67 @@ def test_valid_non_unique_append(int_coll_non_unique: IntCollection):
     int_coll_non_unique.append(26)
     int_coll_non_unique.append(26)
     assert int_coll_non_unique._collection == [26, 26]
+
+def test_reverse(int_coll: IntCollection, valid_int_list):
+    """Test the ability to reverse the collection."""
+    int_coll.extend(valid_int_list)
+    assert [rev for rev in reversed(int_coll)] == [26, 5, 4, 3, 2, 1]
+
+
+def test_contains(int_coll: IntCollection, valid_int_list):
+    """Test the ability to identify membership in the collection by use of the
+        'in' operator"""
+    int_coll.extend(valid_int_list)
+    assert 26 in int_coll
+
+
+def test_add_valid_collections(valid_int_list, valid_int_list_again):
+    """Test the addition of two valid collections of the same type."""
+    coll1 = IntCollection(valid_int_list)
+    coll2 = IntCollection(valid_int_list_again)
+    combined = coll1 + coll2
+    assert [itm for itm in combined] == valid_int_list + valid_int_list_again
+
+
+def test_add_invalid_collections(valid_int_list, valid_str_list):
+    """Test raising of appropriate exception with the addition of two invalid
+        collections of the differing types."""
+    coll1 = IntCollection(valid_int_list)
+    coll2 = StrCollection(valid_str_list)
+    with pytest.raises(TypeError):
+        combined = coll1 + coll2
+
+
+def test_add_valid_item_to_collection(int_coll):
+    """Test the addition of a valid item to a collection positioned as the left
+        operand."""
+    int_coll = int_coll + 26
+    assert 26 in int_coll
+
+
+def test_add_invalid_item_to_collection(int_coll):
+    """Test the raising of appropriate exception with addition of an invalid
+        item to a collection positioned as the left operand."""
+    with pytest.raises(TypeError):
+        int_coll = int_coll + "Invalid string"
+
+
+def test_radd_valid_item_to_collection(int_coll):
+    """Test the addition of a valid item to a collection positioned as the
+        right operand."""
+    int_coll = 26 + int_coll
+    assert 26 in int_coll
+
+
+def test_radd_invalid_item_to_collection(int_coll):
+    """Test the raising of appropriate exception with addition of an invalid
+        item to a collection positioned as the right operand."""
+    with pytest.raises(TypeError):
+        int_coll = "Invalid string" + int_coll
+
+def test_len_valid_collection(int_coll: IntCollection, valid_int_list):
+    int_coll.extend(valid_int_list)
+    assert len(int_coll) == len(valid_int_list)
+
+def test_len_empty_collection(int_coll: IntCollection):
+    assert len(int_coll) == 0
